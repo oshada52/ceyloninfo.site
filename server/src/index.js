95ad77ce886cc89a722c3a1963331b0a52573f1f
Dilshan Oshada <doshada32@gmail.com>
2023-08-12 10:11:17 +0530

    add dot env configs

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import app from './app.js';
+import 'dotenv/config';
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server running on ${port}.`);
