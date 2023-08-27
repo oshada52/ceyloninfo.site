696aa78d30e1a2b59bb106571813d0e965fad176
Dilshan Oshada <doshada32@gmail.com>
2023-08-27 15:11:14 +0530

    Code refactoring

--- client/main.js
+++ client/main.js
@@ -1,91 +1,134 @@
+// Set the Mapbox API access token
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
+// Define geographical bounds for the map
 const bounds = [
-  [75.312182, 3.983108], // southwestern corner of the bounds
-  [84.714728, 12.572659], // northeastern corner of the bounds
+  [78.016975, 3.544604], // southwestern corner of the bounds
+  [83.096048, 11.467576], // northeastern corner of the bounds
 ];
 
+// Create a new Mapbox instance
 const map = new mapboxgl.Map({
-  container: 'map', // container ID
+  container: 'map', // HTML element id where the map will be rendered
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: 'mapbox://styles/dwoshada/cllgq36y8016m01qp6gu69id9', // style URL
   center: [80.738662, 7.840042], // starting position [lng, lat]
-  zoom: 7,
+  zoom: 3,
   maxBounds: bounds,
 });
 
-async function getData() {
-  const response = await fetch('https://ceyloninfo.site/api/locations');
-
-  return response.json();
+// fetches GeoJSON data from the database
+async function getGeojson() {
+  const response = await fetch('https://ceyloninfo.site/api/geojson').then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.json();
+    }
+  );
+  return response;
 }
 
-async function getLocations() {
-  const response = await fetch('https://ceyloninfo.site/api/geojson');
-
-  return response.json();
+//  fetching location information from the backend or session storage
+async function getAllLocationsInfo() {
+  // Check if locations are already in session storage
+  if (!sessionStorage.getItem('locations')) {
+    // Fetch locations from the backend
+    const response = await fetch('https://ceyloninfo.site/api/locations').then(
+      (response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+      }
+    );
+    sessionStorage.setItem('locations', JSON.stringify(response));
+    return response;
+  } else {
+    // Return locations from session storage
+    return JSON.parse(sessionStorage.getItem('locations'));
+  }
 }
 
-getLocations().then((locations) => {
+// Finds location details based on the location name
+async function findLocationDetails(name) {
+  const result = await getAllLocationsInfo().then((response) => {
+    return response.find((item) => item.name == name);
+  });
+  return result;
+}
+
+// DOM element queries for displaying location details
+const infoTitle = document.querySelector('.info-title');
+const infoCity = document.querySelector('.info-city');
+const infoDes = document.querySelector('.info-des');
+const gmapsLink = document.querySelector('.gmaps-link');
+const div = document.querySelector('.location-info');
+
+// Displays location details in the DOM
+function showLocationDetails(location) {
+  infoTitle.innerHTML = location.name;
+  infoCity.innerHTML = `${location.city}, ${location.province} Province`;
+  infoDes.innerHTML = location.description;
+  gmapsLink.href = location.googlemaps;
+  div.style.display = 'flex';
+}
+
+// Adds marker and layer to the map for a given location
+function addMarkerAndLayer(map, location, i) {
+  // Load custom marker image
+  map.loadImage(`${location.icon}`, (error, image) => {
+    if (error) throw error;
+    map.addImage(`custom-marker${i}`, image);
+    // Add a GeoJSON source with points for the location
+    map.addSource(`points${i}`, {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: location.geojson,
+      },
+    });
+    // Add a symbol layer
+    map.addLayer({
+      id: `points${i}`,
+      type: 'symbol',
+      source: `points${i}`,
+      layout: {
+        'icon-image': `custom-marker${i}`,
+        // get the title name from the source's "title" property
+        'text-field': ['get', 'title'],
+        'text-font': ['Overpass SemiBold', 'Arial Unicode MS Bold'],
+        'text-offset': [0, 1.7],
+        'text-size': 12,
+        'text-letter-spacing': 0.02,
+        'text-anchor': 'top',
+      },
+    });
+  });
+
+  // Click event handler for the marker
+  map.on('click', `points${i}`, async (e) => {
+    const markerName = e.features[0].properties.title;
+    const locationDetails = await findLocationDetails(markerName);
+    map.flyTo({
+      center: [locationDetails.longitude, locationDetails.latitude],
+      zoom: 14,
+      essential: true,
+    });
+
+    // Show location details on the DOM
+    showLocationDetails(locationDetails);
+  });
+}
+
+// Fetch GeoJSON data and add markers and layers to the map
+getGeojson().then((response) => {
   map.on('load', () => {
     map.resize();
-    // Add an image to use as a custom marker
-    for (let i = 0; i < locations.length; i++) {
-      // console.log([locations[i].longitude, locations[i].id]);
-      map.loadImage(`${locations[i].icon}`, (error, image) => {
-        if (error) throw error;
-        map.addImage(`custom-marker${i}`, image);
-        // Add a GeoJSON source with 2 points
-        map.addSource(`points${i}`, {
-          type: 'geojson',
-          data: {
-            type: 'FeatureCollection',
-            features: locations[i].geojson,
-          },
-        });
-        // Add a symbol layer
-        map.addLayer({
-          id: `points${i}`,
-          type: 'symbol',
-          source: `points${i}`,
-          layout: {
-            'icon-image': `custom-marker${i}`,
-            // get the title name from the source's "title" property
-            'text-field': ['get', 'title'],
-            'text-font': ['Overpass SemiBold', 'Arial Unicode MS Bold'],
-            'text-offset': [0, 1.7],
-            'text-size': 12,
-            'text-letter-spacing': 0.02,
-            'text-anchor': 'top',
-          },
-        });
-      });
-
-      const x = map.on('click', `points${i}`, (e) => {
-        let locs = e.features[0].properties.title;
-        getData().then((data) => {
-          for (let y = 0; y < data.length; y++) {
-            if (data[y].name == locs) {
-              map.flyTo({
-                center: [data[y].longitude, data[y].latitude],
-                zoom: 14,
-                essenstial: true,
-              });
-              const infoTitle = document.querySelector('.info-title');
-              infoTitle.innerHTML = data[y].name;
-              const infoCity = document.querySelector('.info-city');
-              infoCity.innerHTML = `${data[y].city}, ${data[y].province} Province`;
-              const infoDes = document.querySelector('.info-des');
-              infoDes.innerHTML = data[y].description;
-              const gmapsLink = document.querySelector('.gmaps-link');
-              gmapsLink.href = data[y].googlemaps;
-              const div = document.querySelector('.location-info');
-              div.style.display = 'flex';
-              // document.querySelector('.location-info').style.display = 'flex';
-            }
-          }
-        });
-      });
-    }
+    // Iterate through locations and add markers and layers
+    response.forEach((location, i) => {
+      addMarkerAndLayer(map, location, i);
+    });
   });
 });
